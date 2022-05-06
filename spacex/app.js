e29717684875720465dfb6010d5c103ce5f36b27
Kshishtawy <kareememadelshishtawy@gmail.com>
2022-05-06 21:27:07 +0200

    Deleted markers

--- spacex/app.js
+++ spacex/app.js
@@ -22,17 +22,17 @@ tl.from(".s1", {y: -100, opacity: 0, duration: 2}, "-=2");
 
 tl.from(".spacexLogo", {y: -100, opacity: 0, duration: 2}, "-=3"); // section 1 ends here
 
-gsap.from(".s2", {scrollTrigger: { trigger: ".s2", start: "top 90%", markers: true}, y: 100, opacity: 0, duration: 1}); //scroll trigger starts here
+gsap.from(".s2", {scrollTrigger: { trigger: ".s2", start: "top 90%"}, y: 100, opacity: 0, duration: 1}); //scroll trigger starts here
 
-gsap.from(".s2Stag", {scrollTrigger: { trigger: ".s2Stag", start: "top 60%", markers: true}, stagger: 0.4, y: 100, opacity: 0, duration: 1}); // the awesome stagger animation
+gsap.from(".s2Stag", {scrollTrigger: { trigger: ".s2Stag", start: "top 60%"}, stagger: 0.4, y: 100, opacity: 0, duration: 1}); // the awesome stagger animation
 
-gsap.from(".failvid", {scrollTrigger: { trigger: ".failvid", start: "top 90%", markers: true}, y: 100, opacity: 0, duration: 1}); // section 3 starts here
+gsap.from(".failvid", {scrollTrigger: { trigger: ".failvid", start: "top 90%"}, y: 100, opacity: 0, duration: 1}); // section 3 starts here
 
-gsap.from(".falconCont", {scrollTrigger: { trigger: ".falconCont", start: "top center", markers: true}, x: 100, opacity: 0, duration: 1});
+gsap.from(".falconCont", {scrollTrigger: { trigger: ".falconCont", start: "top center"}, x: 100, opacity: 0, duration: 1});
 
-gsap.from(".faconTextCont", {scrollTrigger: { trigger: ".faconTextCont", start: "top center", markers: true}, x: -100, opacity: 0, duration: 1});
+gsap.from(".faconTextCont", {scrollTrigger: { trigger: ".faconTextCont", start: "top center"}, x: -100, opacity: 0, duration: 1});
 
-gsap.from(".s4", {scrollTrigger: { trigger: ".s4", start: "top 90%", markers: true}, x: -100, opacity: 0, duration: 5});
+gsap.from(".s4", {scrollTrigger: { trigger: ".s4", start: "top 90%"}, x: -100, opacity: 0, duration: 5});
 
-gsap.from(".s4Stag", {scrollTrigger: { trigger: ".s4", start: "top 30%", markers: true}, y: 100, stagger: 0.4, opacity: 0, duration: 1});
+gsap.from(".s4Stag", {scrollTrigger: { trigger: ".s4", start: "top 30%"}, y: 100, stagger: 0.4, opacity: 0, duration: 1});
 
