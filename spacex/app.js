16595833b22962921239a46013cb50364ad8f1a7
Kshishtawy <kareememadelshishtawy@gmail.com>
2022-05-06 21:15:09 +0200

    Animations added

--- spacex/app.js
+++ spacex/app.js
@@ -1,8 +1,14 @@
+gsap.registerPlugin(ScrollTrigger);
+
+window.onbeforeunload = function () { // scroll to the top with every refresh
+    window.scrollTo(0, 0);
+  }
+
 const tl = gsap.timeline({defaults: {ease: "power1.out"}});
 
 tl.from(".introLogo", {y: 200, opacity: 0, duration: 2 });
 
-tl.to(".introLogo", {y: -100, opacity: 0, duration: 1 });
+tl.to(".introLogo", {y: -100, scale: 0.5, opacity: 0, duration: 1 });
 
 tl.from(".slider", {y: "100%", duration: 2}, "-=1"); 
 
@@ -14,4 +20,19 @@ tl.from(".elonPic", {x: 100, opacity: 0, duration: 2}, "-=2");
 
 tl.from(".s1", {y: -100, opacity: 0, duration: 2}, "-=2");
 
-tl.from(".spacexLogo", {y: -100, opacity: 0, duration: 2}, "-=3");
+tl.from(".spacexLogo", {y: -100, opacity: 0, duration: 2}, "-=3"); // section 1 ends here
+
+gsap.from(".s2", {scrollTrigger: { trigger: ".s2", start: "top 90%", markers: true}, y: 100, opacity: 0, duration: 1}); //scroll trigger starts here
+
+gsap.from(".s2Stag", {scrollTrigger: { trigger: ".s2Stag", start: "top 60%", markers: true}, stagger: 0.4, y: 100, opacity: 0, duration: 1}); // the awesome stagger animation
+
+gsap.from(".failvid", {scrollTrigger: { trigger: ".failvid", start: "top 90%", markers: true}, y: 100, opacity: 0, duration: 1}); // section 3 starts here
+
+gsap.from(".falconCont", {scrollTrigger: { trigger: ".falconCont", start: "top center", markers: true}, x: 100, opacity: 0, duration: 1});
+
+gsap.from(".faconTextCont", {scrollTrigger: { trigger: ".faconTextCont", start: "top center", markers: true}, x: -100, opacity: 0, duration: 1});
+
+gsap.from(".s4", {scrollTrigger: { trigger: ".s4", start: "top 90%", markers: true}, x: -100, opacity: 0, duration: 5});
+
+gsap.from(".s4Stag", {scrollTrigger: { trigger: ".s4", start: "top 30%", markers: true}, y: 100, stagger: 0.4, opacity: 0, duration: 1});
+
